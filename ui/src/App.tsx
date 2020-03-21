a27cc9de2fbc2283ec7c3bbc00c63256c1436aa3
blondieboi <48223502+blondieboi@users.noreply.github.com>
2020-03-21 21:44:12 +0100

    added react router dom and created sign in and up (#1)

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import SignIn from "./Views/SignIn";
+import SignUp from "./Views/SignUp";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/loggain">
+            <SignIn />
+          </Route>
+          <Route exact path="/registrera">
+            <SignUp />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
