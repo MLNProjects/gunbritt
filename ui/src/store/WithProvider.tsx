08506a00dcb71de36b452e104004a14be0faf69e
Mattias Mucherie <mattias.mattias.m@gmail.com>
2020-03-21 21:53:06 +0100

    Add store (#2)
    * Add Store
    
    * Fix up store
    
    * Add reducer to own fiel

---
+++ ui/src/store/WithProvider.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const withProvider = (Provider: any) => (Component: any) => (props: any) => (
+  <Provider>
+    <Component {...props} />
+  </Provider>
+);
+
+export default withProvider;
---
+++ ui/src/store/store.tsx
@@ -0,0 +1,17 @@
+import React, { Dispatch, Reducer } from "react";
+
+const createStore = <S extends {}, A extends {}>(reducer: Reducer<S, A>, initialState: S) => {
+  const contextValues: [S, Dispatch<A>] = [initialState, action => {}];
+
+  const Context = React.createContext<[S, Dispatch<A>]>(contextValues);
+
+  const Provider: React.FC = (props: any) => {
+    const store = React.useReducer(reducer, initialState);
+
+    return <Context.Provider value={store}>{props.children}</Context.Provider>;
+  };
+
+  return { Context, Provider, Consumer: Context.Consumer };
+};
+
+export default createStore;
